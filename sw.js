adf7bb8b394a20684c65c752dca3857de3f71034
SFappli <schultzflorian.sf@gmail.com>
2026-07-02 09:50:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service worker Swanne. — cache hors-ligne + mises à jour automatiques */
-const CACHE = "swanne-v47";
+const CACHE = "swanne-v48";
 const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-maskable-512.png"];
 
 self.addEventListener("install", e => {
